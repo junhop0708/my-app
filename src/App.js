33615cd5e583c5a18119d251d101fda0969ac493
JUNHO <junhop0708@gmail.com>
2022-12-12 22:54:49 +0900

    React 기본문법 정리

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+// 0. React 엔진- 데이터 변경을 감지해서 UI를 그려주는 엔진
+// 1. 실행과정 (index.html) - Single Page Application (SPA)
+// 2. JSX 문법 : javascript에 HTML을 작성하는 문법
+// 3. 바벨 (자바스크립트 ES5) -> ES6
+// React에서 페이지를 이동한다는 건 화면을 변경하는게 아니라, body의 내용을 바꾸는 것뿐임
+
+// (1) React는 무조건 하나의 DOM만 return을 해준다!
+// (2) 변수선언은 let 혹은 const로만 해야한다!
+// (3) JSX에서는 if 문은 사용이 불가능, 삼항연산자는 사용 가능 (조건 ? 값(true) : 값(false))
+// (4) 조건부 렌더링 (조건 && 값(true))
+// (5) css디자인
+//     - 내부에 적는 방법
+//     - 외부 파일에 적는 방법
+//     - 라이브러리 사용 (부트스트랩, component-styled)
+
+let a = 10; // 변수
+const b = 20; // 상수
+
+// 대괄호 : [], 중괄호 : {}, 소괄호 : ()
+// return 문안에 JSX가 한줄로 작성되어있으면  소괄호() 가 필요없음
+function App() {
+  let c;
+  let d = undefined;
+
+  console.log(1, c, d);
+
+  const myStyle = {
+    color: 'red',
+  };
+
+  return (
+    <div>
+      <div style={myStyle}>
+        안녕 {a === 10 ? '10입니다.' : '10이 아닙니다.'}
+      </div>
+      <h1 className="box-style">해딩태그 {b === 20 && '20입니다.'}</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// npm start를 하면 index.js가 실행이 됨
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
