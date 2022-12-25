4aa1d844e533c0543b1ba81b8280d2d54a54c1fc
JUNHO <junhop0708@gmail.com>
2022-12-25 22:11:35 +0900

    useEffect 개념정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { cleanup } from '@testing-library/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Third from './aa/Third';
 import './App.css';
 import Sub from './Sub';
@@ -23,12 +23,42 @@ import { num } from './Sub';
 // 대괄호 : [], 중괄호 : {}, 소괄호 : ()
 // return 문안에 JSX가 한줄로 작성되어있으면  소괄호() 가 필요없음
 function App() {
+  const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
+
+  const download = () => {
+    // 다운로드 받고 (통신)
+    let downloadData = 5; // 가정
+    setData(downloadData);
+  };
+
+  // 실행시점 :
+  // (1) App() 함수가 최초 실행될 때 (Mount 될 때) 그림이 최초에 그려질 때
+  // (2) 상태 변수가 변경될 때 (그게 dependencyList에 등록되어 있어야함)
+  // (3) 의존리스트 관리를 할 수 있다.
+  useEffect(() => {
+    console.log('useEffect 실행됨');
+    download();
+  }, [search]); // search가 변경이 되면 useEffect가 실행 (초기값인 5로 다시 셋팅)
+
   return (
     <div>
-      <Sub />
-      <Third />
-      {num}
-      Hello World
+      <h1>검색 </h1>
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색하기
+      </button>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
